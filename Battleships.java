3b201f034d6757018bb8b7dd4e16673f3f3098f0
Huiyao Zheng <huiyaozheng@gmail.com>
2017-11-25 20:17:28 +0000

    Place ship fixed

--- Battleships.java
+++ Battleships.java
@@ -6,7 +6,9 @@ import java.util.Random;
 import static java.lang.Math.abs;
 
 public class Battleships {
-	/** Random number generator for our auto play game strategy. */
+    /**
+     * Random number generator for our auto play game strategy.
+     */
     static Random r = new Random();
 
     private static float[][] priorities = null;
@@ -14,31 +16,25 @@ public class Battleships {
     private static int boardWidth;
     private static int boardHeight;
     private static int[][] placement = null;
-
-    private static class Shot {int row; int col; Shot(int a, int b){row = a; col = b;}}
     private static Shot lastShot = null;
-    private static boolean trackingShip = false;
-    private static int direction = 0;
-    private static int Vertical = 1;
-    private static int Horiztonal = 2;
-    private static int[][] localShape = {{-1,0},{0,-1},{0,1},{1,0}};
+    private static int[][] localShape = {{-1, 0}, {0, -1}, {0, 1}, {1, 0}};
     private static Shot trackOrigin;
-    private static int[][] transitions = {{1,1},{3,1},{6,3},{7,4},{8,5},{9,1},{6,3},{7,1},{8,5},{9,1}};
+    private static int[][] transitions = {{1, 1}, {2, 1}, {6, 3}, {7, 4}, {8, 5}, {9, 1}, {6, 3}, {7, 1}, {8, 5}, {9, 1}};
     private static int stateCount = 10;
     private static int currentState = 0;
     private static String shotResult;
+
     /* Use this method to define the bot's strategy.
      */
     public static BattleshipsMove calculateMove(MainWindow.GameState state) {
-        if(state.Round==0) {
+        if (state.Round == 0) {
             boardHeight = state.MyBoard.size();
             boardWidth = state.MyBoard.get(0).size();
             priorities = new float[boardHeight][boardWidth];
             placement = new int[boardHeight][boardWidth];
-            for(int i = 0; i < boardHeight; ++i){
-                for(int j = 0; j < boardWidth; ++j){
-                    priorities[i][j] = 1;
-                    if (state.MyBoard.get(i).get(j).equals("")){
+            for (int i = 0; i < boardHeight; ++i) {
+                for (int j = 0; j < boardWidth; ++j) {
+                    if (state.MyBoard.get(i).get(j).equals("")) {
                         placement[i][j] = 100;
                     }
                     if (state.MyBoard.get(i).get(j).equals(""))
@@ -47,143 +43,94 @@ public class Battleships {
                         priorities[i][j] = 0;
                 }
             }
-            trackingShip = false;
             currentState = 0;
             shotResult = "";
-        	return placeShips(state);
-        }
-        else {
-
-        	return findShipWithStateMachine(state);
+            return placeShips(state);
+        } else {
+            return findShipWithStateMachine(state);
         }
     }
 
-    private static ArrayList<Integer> findMax() {
-        ArrayList<Integer> maximum = new ArrayList<>();
-        int maxVal = 0;
-        for (int i = 0; i < placement.length; i++) {
-            for (int j = 0; j < placement[0].length; j++) {
-                if (maxVal < placement[i][j]) {
-                    maxVal = placement[i][j];
-                    maximum = new ArrayList<>();
-                    maximum.add(i * placement[0].length + j);
-                } else if (maxVal == placement[i][j]) {
-                    maximum.add(i * placement[0].length + j);
+    private static ArrayList<ShipPositionToBe> findValidPosition(int length, ArrayList<ArrayList<String>> board) {
+        ArrayList<ShipPositionToBe> result = new ArrayList<>();
+        for (int i = 0; i < boardHeight - length + 1; i++) {
+            for (int j = 0; j < boardWidth; j++) {
+                boolean able = true;
+                int score = 0;
+                for (int l = 0; l < length; l++) {
+                    String square = board.get(i + l).get(j);
+                    if (!square.equals("")) {
+                        able = false;
+                        break;
+                    }
+                    score += placement[i + l][j];
                 }
+                if (able) result.add(new ShipPositionToBe(Character.toString((char) (i + 65)), j, "V", score));
             }
         }
-        return maximum;
-    }
-
-    private static ArrayList<ShipPosition> findValidPosition (int length, ArrayList<ArrayList<String>> board) {
-        ArrayList<ShipPosition> result = new ArrayList<>();
-        for (int i = 0; i < board.size(); i++) {
-            for (int j = 0; j < board.get(0).size(); j++) {
-                if(i + length - 1 >= board.size()) { //If ship doesn't fit within board boundaries VERTICALLY
-                    continue;
-                }
-                for(int l=0; l < length; l++) {
-                    String square = board.get(i+l).get(j);
-                    if(!square.equals("")) {
-                        continue;
+        for (int i = 0; i < boardHeight; i++) {
+            for (int j = 0; j < boardWidth - length + 1; j++) {
+                boolean able = true;
+                int score = 0;
+                for (int l = 0; l < length; l++) {
+                    String square = board.get(i).get(j + l);
+                    if (!square.equals("")) {
+                        able = false;
+                        break;
                     }
+                    score += placement[i][j + l];
                 }
-                result.add(new ShipPosition(Character.toString((char)(i+65)),j,"V"));
-            }
-        }
-        for (int i = 0; i < board.size(); i++) {
-            for (int j = 0; j <board.get(0).size(); j++) {
-                if(j + length - 1 >= board.get(0).size()) { //If ship doesn't fit within board boundaries
-                    continue;
-                }
-                for(int l=0; l < length; l++) {
-                    String square = board.get(i).get(j+l);
-                    if(!square.equals("")) {
-                        continue;
-                    }
-                }
-                result.add(new ShipPosition(Character.toString((char) (i + 65)), j, "H"));
+                if (able) result.add(new ShipPositionToBe(Character.toString((char) (i + 65)), j, "H", score));
             }
         }
         return result;
     }
 
-    private static ShipPosition findBestPosition(ArrayList<ShipPosition> validPositions, int length) {
+    private static ShipPosition findBestPosition(ArrayList<ShipPositionToBe> validPositions) {
         int max = 0;
         ShipPosition result = null;
-        for (int i = 0; i < validPositions.size(); i++) {
-            int sum = 0;
-            if (validPositions.get(i).Orientation.equals("V")) {
-                for (int j = 0; j < length; j++) {
-                    sum += placement[validPositions.get(i).Row.toCharArray()[0] -65 + j][validPositions.get(i).Column];
-                }
-                if (max < sum) {
-                    max = sum;
-                    result = validPositions.get(i);
-                }
+        for (ShipPositionToBe i : validPositions) {
+            if (i.score > max) {
+                max = i.score;
+                result = i;
             }
         }
         return result;
     }
 
     private static BattleshipsMove placeShips(MainWindow.GameState state) {
-    	ArrayList<ShipPosition> placement = new ArrayList<ShipPosition>();
-    	int row = 0, column = 0;
-    	String orientation = "";
-	    for (int i = 0; i < state.Ships.size(); i++) {
-	    	boolean placed = false;
-	    	while(!placed) {
-	    		ArrayList<ArrayList<String>> board = state.MyBoard;
-	    		ShipPosition newPlace = findBestPosition(findValidPosition(state.Ships.get(i),state.MyBoard), state.Ships.get(i));
-	    		row = newPlace.Row.toCharArray()[0] - 65;
-	    		column = newPlace.Column;
-	    		orientation = newPlace.Orientation;
-	    		/*
-	    		Put code on how to modify placement[][] priority here, based on the new ship to be placed.
-	    		 */
-	    		board = attemptShipPlacement(row, column, state.MyBoard, state.Ships.get(i), orientation, i);
-	    		if(board != null) {
-	    			placed = true;
-	    			state.MyBoard = board;
-	    		}
-	    	}
-	    	placement.add(new ShipPosition(Character.toString((char)(row+65)), column+1, orientation));
-	    }
-	    return new BattleshipsMove(placement, "", 0);
-    }
-    
-    private static BattleshipsMove findShips(MainWindow.GameState state) {
-    	boolean placed = false;
-		int row=0, column=0;
-    	while(!placed) {
-    		ArrayList<ArrayList<String>> board = state.MyBoard;
-    		row = r.nextInt(board.size());
-    		column = r.nextInt(board.get(0).size());
-    		if(state.OppBoard.get(row).get(column).equals("") || state.OppBoard.get(row).get(column).equals("L")) {
-    			placed = true;
-    		}
-    	}
-    	return new BattleshipsMove(null, Character.toString((char)(row+65)), column+1);
+        ArrayList<ShipPosition> placement = new ArrayList<ShipPosition>();
+        for (int i = 0; i < state.Ships.size(); i++) {
+            ShipPosition newPlace = findBestPosition(findValidPosition(state.Ships.get(i), state.MyBoard));
+            ArrayList<ArrayList<String>> board;
+            board = attemptShipPlacement(newPlace.Row.charAt(0) - 'A', newPlace.Column, state.MyBoard, state.Ships.get(i), newPlace.Orientation, i);
+            state.MyBoard = board;
+            newPlace.Column++;
+            placement.add(newPlace);
+        }
+        return new BattleshipsMove(placement, "", 0);
     }
 
-    private static void firstHit(int row, int col) {
-        for (int i = 0; i < 4; ++i) {
-            int newRow = row + localShape[i][0];
-            int newCol = col + localShape[i][1];
-            if (newRow >= 0 && newRow < boardHeight && newCol >= 0 && col + newCol < boardWidth) {
-                if (priorities[newRow][newCol] != 0) {
-                    priorities[newRow][newCol] += 0.2;
-                }
+    private static BattleshipsMove findShips(MainWindow.GameState state) {
+        boolean placed = false;
+        int row = 0, column = 0;
+        while (!placed) {
+            ArrayList<ArrayList<String>> board = state.MyBoard;
+            row = r.nextInt(board.size());
+            column = r.nextInt(board.get(0).size());
+            if (state.OppBoard.get(row).get(column).equals("") || state.OppBoard.get(row).get(column).equals("L")) {
+                placed = true;
             }
         }
+        return new BattleshipsMove(null, Character.toString((char) (row + 65)), column + 1);
     }
 
     private static Shot randomShot() {
         int maxCol = 0;
         int maxRow = 0;
         float maxP = 0;
-        for(int i = 0; i < boardHeight; ++i){
-            for(int j = 0; j <boardWidth; ++j){
+        for (int i = 0; i < boardHeight; ++i) {
+            for (int j = 0; j < boardWidth; ++j) {
                 if (priorities[i][j] > maxP) {
                     maxRow = i;
                     maxCol = j;
@@ -195,21 +142,21 @@ public class Battleships {
     }
 
     private static BattleshipsMove executeS() {
-        switch (currentState){
+        switch (currentState) {
             case 1:
                 lastShot = randomShot();
                 break;
             case 2:
-                lastShot = new Shot(lastShot.row - 1, lastShot.col);
+                lastShot = new Shot(trackOrigin.row - 1, trackOrigin.col);
                 break;
             case 3:
-                lastShot = new Shot(lastShot.row + 1, lastShot.col);
+                lastShot = new Shot(trackOrigin.row + 1, trackOrigin.col);
                 break;
             case 4:
-                lastShot = new Shot(lastShot.row, lastShot.col - 1);
+                lastShot = new Shot(trackOrigin.row, trackOrigin.col - 1);
                 break;
             case 5:
-                lastShot = new Shot(lastShot.row, lastShot.col + 1);
+                lastShot = new Shot(trackOrigin.row, trackOrigin.col + 1);
                 break;
             case 6:
                 lastShot = new Shot(lastShot.row - 1, lastShot.col);
@@ -228,10 +175,10 @@ public class Battleships {
         System.out.print(lastShot.row);
         System.out.println(lastShot.col);
         priorities[lastShot.row][lastShot.col] = 0;
-        return new BattleshipsMove(null, Character.toString((char)(lastShot.row + 65)), lastShot.col + 1);
+        return new BattleshipsMove(null, Character.toString((char) (lastShot.row + 65)), lastShot.col + 1);
     }
 
-    private static boolean canShoot(int row, int col){
+    private static boolean canShoot(int row, int col) {
         if (row >= 0 && row < boardHeight && col >= 0 && col < boardWidth) {
             return priorities[row][col] != 0;
         } else {
@@ -240,17 +187,17 @@ public class Battleships {
     }
 
     private static boolean available(int state) {
-        switch (state){
+        switch (state) {
             case 1:
                 return true;
             case 2:
-                return canShoot(lastShot.row - 1, lastShot.col);
+                return canShoot(trackOrigin.row - 1, trackOrigin.col);
             case 3:
-                return canShoot(lastShot.row + 1, lastShot.col);
+                return canShoot(trackOrigin.row + 1, trackOrigin.col);
             case 4:
-                return canShoot(lastShot.row, lastShot.col - 1);
+                return canShoot(trackOrigin.row, trackOrigin.col - 1);
             case 5:
-                return canShoot(lastShot.row, lastShot.col + 1);
+                return canShoot(trackOrigin.row, trackOrigin.col + 1);
             case 6:
                 return canShoot(lastShot.row - 1, lastShot.col);
             case 7:
@@ -265,12 +212,13 @@ public class Battleships {
     }
 
     private static BattleshipsMove findShipWithStateMachine(MainWindow.GameState state) {
-        if (currentState == 0){
+        if (currentState == 0) {
             currentState = 1;
             return executeS();
         } else {
             shotResult = state.OppBoard.get(lastShot.row).get(lastShot.col);
             if (shotResult.equals("H")) {
+                if (currentState == 1) trackOrigin = new Shot(lastShot.row, lastShot.col);
                 currentState = transitions[currentState][0];
             } else if (shotResult.equals("M")) {
                 currentState = transitions[currentState][1];
@@ -283,36 +231,54 @@ public class Battleships {
         }
     }
 
+    private static ArrayList<ArrayList<String>> attemptShipPlacement(int i, int j, ArrayList<ArrayList<String>> board, int length, String orientation, int shipNum) {
+        if (orientation.equals("V")) { //Vertical
+            if (i + length - 1 >= board.size()) { //If ship doesn't fit within board boundaries
+                return null;
+            }
+            for (int l = 0; l < length; l++) {
+                String square = board.get(i + l).get(j);
+                if (!square.equals("")) {
+                    return null;
+                }
+            }
+            for (int l = 0; l < length; l++) {
+                board.get(i + l).set(j, Integer.toString(shipNum));
+            }
+            return board;
+        } else { //Horizontal
+            if (j + length - 1 >= board.get(0).size()) { //If ship doesn't fit within board boundaries
+                return null;
+            }
+            for (int l = 0; l < length; l++) {
+                String square = board.get(i).get(j + l);
+                if (!square.equals("")) {
+                    return null;
+                }
+            }
+            for (int l = 0; l < length; l++) {
+                board.get(i).set(j + l, Integer.toString(shipNum));
+            }
+            return board;
+        }
+    }
 
-    private static ArrayList<ArrayList<String>> attemptShipPlacement(int i, int j, ArrayList<ArrayList<String>> board, int length, String orientation, int shipNum){
-    	if(orientation.equals("V")) { //Vertical
-    		if(i + length - 1 >= board.size()) { //If ship doesn't fit within board boundaries
-    			return null;
-    		}
-    		for(int l=0; l < length; l++) {
-    			String square = board.get(i+l).get(j);
-    			if(!square.equals("")) {
-    				return null;
-    			}
-    		}
-    		for(int l=0; l < length; l++) {
-    			board.get(i+l).set(j, Integer.toString(shipNum));
-    		}
-    		return board;
-    	} else { //Horizontal
-    		if(j + length - 1 >= board.get(0).size()) { //If ship doesn't fit within board boundaries
-    			return null;
-    		}
-    		for(int l=0; l < length; l++) {
-    			String square = board.get(i).get(j+l);
-    			if(!square.equals("")) {
-    				return null;
-    			}
-    		}
-    		for(int l=0; l < length; l++) {
-    			board.get(i).set(j+l, Integer.toString(shipNum));
-    		}
-    		return board;
-    	}
+    private static class ShipPositionToBe extends ShipPosition {
+        int score;
+
+        ShipPositionToBe(String row, int column, String orientation, int score) {
+            super(row, column, orientation);
+            this.score = score;
+        }
+    }
+
+    private static class Shot {
+        int row;
+        int col;
+
+        Shot(int a, int b) {
+            row = a;
+            col = b;
+        }
     }
 }
